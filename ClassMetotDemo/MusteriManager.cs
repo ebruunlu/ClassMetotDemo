af45f4782e7fd32dccd92aee1528701845370b98
Ebru Ünlü <ebruunlu3@gmail.com>
2022-12-06 00:39:49 +0300

    completed

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Adi + " Müşterisi eklendi.");
+        }
+
+        public void Sil(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Adi +" "+ musteri.Soyadi + " Müşterisi silindi.");
+        }
+        public void Listele(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Adi+" "+ musteri.Soyadi+ " "+ musteri.Tckn);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             Musteri musteri1= new Musteri();
+            musteri1.Adi = "Ömer Faruk";
+            musteri1.Soyadi = "Geriş";
+            musteri1.Tckn= "38763567999";
+
+            Musteri musteri2= new Musteri();
+            musteri2.Adi = "Ebru";
+            musteri2.Soyadi = "Geriş";
+            musteri2.Tckn = "387867555";
+
+            MusteriManager yonetim= new MusteriManager();
+            yonetim.Ekle(musteri1);
+            yonetim.Ekle(musteri2);
+
+            yonetim.Listele(musteri1);
+            yonetim.Listele(musteri2);
+
+            yonetim.Sil(musteri1);
+            yonetim.Sil(musteri2);
+
         }
+
     }
 }
